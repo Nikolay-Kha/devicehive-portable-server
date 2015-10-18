47a4bbf9fbb13a0e3841f465eb6a59fc287babed
Nikolay-Khabarov <2xl@mail.ru>
2015-10-18 14:46:10 +0300

    change server java args

--- scripts/devicehive.sh
+++ scripts/devicehive.sh
@@ -7,5 +7,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$PATH:$DIR/java-$(uname -i)/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DIR/java-$(uname -i)/lib
 
-java -Xms128m -Xmx128m -jar $DIR/devicehive-server/devicehive-boot.jar
+$DIR/java-$(uname -i)/bin/java -Xms128m -Xmx128m -jar $DIR/devicehive-server/devicehive-boot.jar \
+	-Dspring.datasource.url=jdbc:postgresql://127.0.0.1:5432/devicehive \
+	-Dspring.datasource.username="postgres" \
+	-Dspring.datasource.password="12345" \
+	-Dmetadata.broker.list=127.0.0.1:9092 \
+	-Dzookeeper.connect=127.0.0.1:2181 \
+	-Dserver.context-path=dh/rest \
+	-Dserver.port=8080
 
